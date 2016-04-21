ce304ef347a5bdbc48e3b2e4b0f83e382974ebb4
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-04-21 16:19:41 -0500

    Add PokemonCell view

---
+++ PokedexXX/PokemonCell.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonCell.swift
+//  PokedexXX
+//
+//  Created by Steven on 4/21/16.
+//  Copyright © 2016 steven. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        nameLabel.text = pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(pokemon.pokedexId)")
+    }
+}
