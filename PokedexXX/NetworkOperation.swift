ab977cfef1223b16d60bc65910cd3ea2bc36a721
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-04-22 00:56:09 -0500

    Setup network operation Alamofire

--- PokedexXX/NetworkOperation.swift
+++ PokedexXX/NetworkOperation.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkOperation {
+    
+    private var _queryURL: NSURL
+    
+    var queryURL: NSURL {
+        return _queryURL
+    }
+    
+    init(url: NSURL) {
+        self._queryURL = url
+    }
+    
+    func downloadJSONFromURL(completion: JSONDictionaryCompletion) {
+        Alamofire.request(.GET, queryURL).responseJSON { (response: Response<AnyObject, NSError>) in
+            if let error = response.result.error {
+                print(error.debugDescription)
+            } else {
+                if let results = response.result.value as? [String: AnyObject] {
+                    completion(results)
+                }
+            }
+        }
+    }
+}
