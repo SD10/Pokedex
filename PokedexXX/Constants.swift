f4e1072ba8e3f3e2b986203bb584c06ef336fb3d
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-04-21 21:35:07 -0500

    Add constants file

---
+++ PokedexXX/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  PokedexXX
+//
+//  Created by Steven on 4/21/16.
+//  Copyright © 2016 steven. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - URLs
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+// MARK: - Segues
+let SHOW_DETAIL = "showPokemonDetail"
+
+
+
