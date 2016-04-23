803b59da39694079c1ec9ccd24f230df8a031c55
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-04-22 21:19:03 -0500

    Add ColorTheme

---
+++ PokedexXX/ColorTheme.swift
@@ -0,0 +1,28 @@
+//
+//  ColorTheme.swift
+//  PokedexXX
+//
+//  Created by Steven on 4/22/16.
+//  Copyright © 2016 steven. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum ColorTheme {
+    case CharamanderRed
+    case SquirtleBlue
+    case BulbasaurGreen
+    case PikachuYellow
+    case OnyxGrey
+    
+    var colorTheme: UIColor {
+        switch self {
+            case .CharamanderRed: return UIColor.charamanderRed()
+            case .SquirtleBlue: return UIColor.squirtleBlue()
+            case .BulbasaurGreen: return UIColor.bulbasaurGreen()
+            case .PikachuYellow: return UIColor.pikachuYellow()
+            case .OnyxGrey: return UIColor.onyxGrey()
+        }
+    }
+}
---
+++ PokedexXX/ParserService.swift
@@ -0,0 +1,9 @@
+//
+//  ParserService.swift
+//  PokedexXX
+//
+//  Created by Steven on 4/22/16.
+//  Copyright © 2016 steven. All rights reserved.
+//
+
+import Foundation
